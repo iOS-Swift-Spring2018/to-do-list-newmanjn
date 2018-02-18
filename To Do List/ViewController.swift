fe38b81b49ed1f2ab2011fc2c58597174532c933
Jess Newman <jessnewman@jesss-macbook-pro.local>
2018-02-18 16:08:49 -0600

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Jess Newman on 2/18/18.
+//  Copyright © 2018 Newman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
